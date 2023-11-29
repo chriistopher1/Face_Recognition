80c6c6f31f20a24facd559f0e3438da45de81850
chriistopher1 <142476784+chriistopher1@users.noreply.github.com>
2023-11-29 22:21:14 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,17 @@ import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
- 
-    
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env.PUBLIC_URL": JSON.stringify(env.PUBLIC_URL),
+    },
     plugins: [react()],
-    base: "/Face_Recognition/",
+    base: "/face-recognition/",
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-  
+  };
 });
